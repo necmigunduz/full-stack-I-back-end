e952ec29d15dd39c0c73480f95f96b4639a52959
necmigunduz <necmigunduz@gmail.com>
2021-10-19 17:13:49 +0300

    Create API V1 recipients controller and add index method with private params

---
+++ app/controllers/api/v1/recipients_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::RecipientsController < ApplicationController
+    def index 
+        recipients = Recipient.all
+        render json: recipients
+    end
+
+    private
+    def recipient_params
+        params.require(:recipient).permit(:name, :mailing_adress, :preferences)
+    end
+end
---
+++ test/controllers/api/recipients_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class Api::RecipientsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
